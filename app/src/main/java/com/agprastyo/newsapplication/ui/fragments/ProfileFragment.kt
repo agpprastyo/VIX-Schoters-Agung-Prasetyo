0f58c8af4ff5c076bcbdf77cedb4241d69ea4f82
Agung Prasetyo <93957986+agpprastyo@users.noreply.github.com>
2023-01-16 08:02:22 +0700

    update button profile

--- app/src/main/java/com/agprastyo/newsapplication/ui/fragments/ProfileFragment.kt
+++ app/src/main/java/com/agprastyo/newsapplication/ui/fragments/ProfileFragment.kt
@@ -9,7 +9,6 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.agprastyo.newsapplication.R
-import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment : Fragment(R.layout.fragment_profile), View.OnClickListener {
     override fun onCreateView(
